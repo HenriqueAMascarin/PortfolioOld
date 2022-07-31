9312c4a9fe8d1d62679ea7272106c7980600cad4
LuckTheGamers <thegamers056@gmail.com>
2022-07-30 23:25:18 -0300

    final parallax 4

--- js/parallax.js
+++ js/parallax.js
@@ -2,12 +2,11 @@ window.addEventListener('deviceorientation',function(e){
     const x = Math.round(e.beta);
     const y = Math.round(e.gamma);
     const z = Math.round(e.alpha);
-
-    //Parallax Effect
+    if(x < 30){
+        //Parallax Effect
     document.getElementsByClassName('circles')[0].style.transform = `translateX(-${x}%)`;
     document.getElementsByClassName('circles')[0].style.transform = `translateX(+${x}%)`;
     document.getElementsByClassName('photo-henrique')[0].style.transform = `translateX(-${x}%)`;
     document.getElementsByClassName('photo-henrique')[0].style.transform = `translateX(+${x}%)`;
-    document.getElementsByClassName('tittle-break')[0].style.transform = `translateX(-${x}%)`;
-    document.getElementsByClassName('tittle-break')[0].style.transform = `translateX(+${x}%)`;
+    }
 });
