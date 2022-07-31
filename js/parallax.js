63fd51f6619b39c346fd35fce5c338600e754bfb
LuckTheGamers <thegamers056@gmail.com>
2022-07-30 23:36:08 -0300

    test

--- js/parallax.js
+++ js/parallax.js
@@ -2,11 +2,15 @@ window.addEventListener('deviceorientation',function(e){
     const x = Math.round(e.beta);
     const y = Math.round(e.gamma);
     const z = Math.round(e.alpha);
-    while(x < 20 && x > -20){
+
+    document.getElementsByClassName('xtxt')[0].innerText = "X-Axis = " + x;
+    document.getElementsByClassName('ytxt')[0].innerText = "Y-Axis = " + y;
+    document.getElementsByClassName('ztxt')[0].innerText = "Z-Axis = " + z;
+    
         //Parallax Effect
     document.getElementsByClassName('circles')[0].style.transform = `translateX(-${x}%)`;
     document.getElementsByClassName('circles')[0].style.transform = `translateX(+${x}%)`;
     document.getElementsByClassName('photo-henrique')[0].style.transform = `translateX(-${x}%)`;
     document.getElementsByClassName('photo-henrique')[0].style.transform = `translateX(+${x}%)`;
-    }
+    
 });
