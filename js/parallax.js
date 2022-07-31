d5d3c593b07bed9b1730102fc1aa9c271f6b733a
LuckTheGamers <thegamers056@gmail.com>
2022-07-30 22:58:52 -0300

    design + scroll + parallax test mobile + favicon + fix bugs

---
+++ js/parallax.js
@@ -0,0 +1,9 @@
+window.addEventListener('deviceorientation',function(e){
+    const x = Math.round(e.beta);
+    const y = Math.round(e.gamma);
+    const z = Math.round(e.alpha);
+
+    //Parallax Effect
+    document.getElementsByClassName('circles')[0].style.transform = `translate(-${x}%,-${y}%)`;
+    document.getElementsByClassName('photo-henrique')[0].style.transform = `translate(-${x}%,-${y}%)`;
+});
