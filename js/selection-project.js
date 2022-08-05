1231d8a65606ffa8fda63f5886ea8dc620da5b30
LuckTheGamers <thegamers056@gmail.com>
2022-08-05 19:38:48 -0300

    title project changes

--- js/selection-project.js
+++ js/selection-project.js
@@ -1,6 +1,6 @@
 const project = document.querySelector(".project-card");
-const select = document.querySelector
+const titleProject = document.querySelector(".title-project")
 project.addEventListener("click", (e) => {
     project.classList.toggle("active");
+    titleProject.classList.toggle("active");
 })
-
