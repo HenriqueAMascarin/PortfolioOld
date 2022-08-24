fb0eaa80b7612e53e68f699d0c20312e5b1ef225
LuckTheGamers <thegamers056@gmail.com>
2022-08-24 20:42:16 -0300

    changes in project

--- js/selection-project.js
+++ js/selection-project.js
@@ -1,50 +1,22 @@
-const project = document.querySelector(".project");
-const project2 = document.querySelector(".project-2");
-const project3 = document.querySelector(".project-3");
+const $project = document.querySelector("#projectDiv");
+const $projectInfo = document.querySelector(".project-info");
+const $projectCard = document.querySelector(".project-card");
+var click = 1;
 
-const titleProject = document.querySelector(".title-act")
-const titleProject2 = document.querySelector(".title-act2")
-const titleProject3 = document.querySelector(".title-act3")
-
-const button = document.querySelector(".button");
-const button2 = document.querySelector(".button2");
-const button3 = document.querySelector(".button3");
+function projectInfoHide(typeDisplay, sizePadding){
+    $projectInfo.style.display = typeDisplay;
+    $projectCard.style.paddingBottom = sizePadding;
+}
+projectInfoHide("none", 0);
 
 //PROJECT DIV CLICK
-project.addEventListener("click", () => {
-    project.classList.toggle("active");
-    titleProject.classList.toggle("active");
-})
-
-// project2.addEventListener("click", () => {
-//     project2.classList.toggle("active");
-//     titleProject2.classList.toggle("active");
-// })
-
-// project3.addEventListener("click", () => {
-//     project3.classList.toggle("active");
-//     titleProject3.classList.toggle("active");
-// })
-
-//PROJECT SELECT BUTTONS
-button.classList.add("active");
-button.addEventListener("click", () => {
-    button2.classList.remove("active");
-    button3.classList.remove("active");
-    button.classList.add("active");
-    project.style.marginLeft = "0%";
-})
-
-button2.addEventListener("click", () => {
-    button.classList.remove("active");
-    button3.classList.remove("active");
-    button2.classList.add("active");
-    project.style.marginLeft = "-125%";
-})
-
-button3.addEventListener("click", () => {
-    button.classList.remove("active");
-    button2.classList.remove("active");
-    button3.classList.add("active");
-    project.style.marginLeft = "-245%";
-})
+$project.addEventListener("click", () => {
+    if(click === 1){
+        projectInfoHide("flex", "270px");
+        click = 0;
+    }else{
+        projectInfoHide("none", "0px");
+        click = 1;
+    }
+    
+});
