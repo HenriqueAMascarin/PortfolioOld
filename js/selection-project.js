ee5041a6c364fe7046aa0d1060a2beea457f2692
LuckTheGamers <thegamers056@gmail.com>
2022-08-25 14:21:21 -0300

    project open info

--- js/selection-project.js
+++ js/selection-project.js
@@ -3,19 +3,20 @@ const $projectInfo = document.querySelector(".project-info");
 const $projectCard = document.querySelector(".project-card");
 var click = 1;
 
-function projectInfoHide(typeDisplay, sizePadding){
-    $projectInfo.style.display = typeDisplay;
+function projectInfoHide(sizePadding,scaleNumber){
+    // $projectInfo.style.display = typeDisplay;
     $projectCard.style.paddingBottom = sizePadding;
+    $projectInfo.style.transform = scaleNumber;
 }
-projectInfoHide("none", 0);
+projectInfoHide("0px", "scaleY(0)");
 
 //PROJECT DIV CLICK
 $project.addEventListener("click", () => {
     if(click === 1){
-        projectInfoHide("flex", "270px");
+        projectInfoHide("270px", "scaleY(1)");
         click = 0;
     }else{
-        projectInfoHide("none", "0px");
+        projectInfoHide("0px" , "scaleY(0)");
         click = 1;
     }
     
