1cb949dd8f7d7a4bfdbcbdd5c3a6e913165573dc
LuckTheGamers <thegamers056@gmail.com>
2022-08-26 19:21:41 -0300

    test padding js

--- js/selection-project.js
+++ js/selection-project.js
@@ -5,21 +5,21 @@ const $title = document.querySelector(".title-project");
 const $projectImage = document.querySelector(".project-image");
 var click = 1;
 
-function projectInfoHide(sizePadding,scaleProject,scaleTitle,brightnessImage){
-    $projectCard.style.paddingBottom = sizePadding;
+function projectInfoHide(scaleProject,scaleTitle,brightnessImage){
+    
     $projectInfo.style.transform = scaleProject;
     $title.style.transform = scaleTitle;
     $projectImage.style.filter = brightnessImage;
 }
-projectInfoHide("0px", "scaleY(0)", "scale(0)", "brightness(100%)");
+projectInfoHide("scaleY(0)", "scale(0)", "brightness(100%)");
 
 //PROJECT DIV CLICK
 $project.addEventListener("click", () => {
     if(click === 1){
-        projectInfoHide("270px", "scaleY(1)", "scale(1)", "brightness(60%)");
+        projectInfoHide("scaleY(1)", "scale(1)", "brightness(60%)");
         click = 0;
     }else{
-        projectInfoHide("0px" , "scaleY(0)", "scale(0)", "brightness(100%)");
+        projectInfoHide("scaleY(0)", "scale(0)", "brightness(100%)");
         click = 1;
     } 
 });
