91ecad3a5569b1da73cbc23e4fa8d65ebd40d4b1
LuckTheGamers <thegamers056@gmail.com>
2022-09-19 00:05:02 -0300

    selection projects js more performance

--- js/selection-projects.js
+++ js/selection-projects.js
@@ -1,70 +1,62 @@
-const projects = document.querySelectorAll(".project-card");
-const ballSelection = document.querySelectorAll(".ball");
-const projectDiv = document.querySelector(".project-card");
-const projectContainer = document.querySelector(".project-container");
-const projectScroll = document.querySelector(".projects");
-const $selectionbuttons = document.querySelector(".selection-buttons");
+(function(){
+    const projects = document.querySelectorAll(".project-card");
+    const ballSelection = document.querySelectorAll(".ball");
+    const projectDiv = document.querySelector(".project-card");
+    const projectContainer = document.querySelector(".project-container");
+    const projectScroll = document.querySelector(".projects");
+    const $selectionbuttons = document.querySelector(".selection-buttons");
 
-var currentLength = 0;
-var showingProjects;
-var select = 3;
+    var currentLength = 0;
+    var showingProjects;
+    var select = 3;
 
-function addClass(current){
-    var x = 0;
-    while(ballSelection[x]){
-        if(ballSelection[x].classList.contains('currentButton')){
-            ballSelection[x].classList.remove('currentButton');
+    function addClass(current){
+        var x = 0;
+        ballSelection.forEach(function(element){
+            element.classList.remove('currentButton');
+        })
+        ballSelection[current].classList.add("currentButton");
+    }
+
+    function offset(){
+        if(projectScroll.clientWidth < 704){
+            showingProjects = 1;
+            $selectionbuttons.classList.add("selectionButtons-js");
+        }else if(projectScroll.clientWidth === 704){ //fixed value of clientWidth
+            showingProjects = 2;
+            $selectionbuttons.classList.add("selectionButtons-js");
+        }else{
+            showingProjects = 3;
+            $selectionbuttons.classList.remove("selectionButtons-js");
         }
-        x++;
     }
-    ballSelection[current].classList.add("currentButton");
-}
+    offset();
 
-function offset(){
-    if(projectScroll.clientWidth < 704){
-        showingProjects = 1;
-        $selectionbuttons.classList.add("selectionButtons-js");
-    }else if(projectScroll.clientWidth === 704){
-        showingProjects = 2;
-        $selectionbuttons.classList.add("selectionButtons-js");
-    }else{
-        showingProjects = 3;
-        $selectionbuttons.classList.remove("selectionButtons-js");
-    }
-}
-offset();
+    $selectionbuttons.addEventListener("click", (e) =>{
+        var currentIndex = Array.prototype.indexOf.call(ballSelection, e.target);
+        if(currentIndex === 1 && currentLength < (projects.length - showingProjects)){
+            currentLength += 1;
+            UpdateCarousel("right");
+            select = currentIndex;
+            addClass(currentIndex);
+        }else if(currentIndex === 0 && currentLength < (projects.length) && currentLength > 0){
+            currentLength -= 1;
+            UpdateCarousel("left");
+            select = currentIndex;
+            addClass(currentIndex);
+        }
+    })
 
-ballSelection[0].addEventListener("click", (e) =>{
-    debugger
-    if(currentLength < (projects.length) && currentLength > 0){
-        currentLength -= 1;
-        UpdateCarousel("left");
+    function UpdateCarousel (direction){
+        var projectWidth = projectDiv.clientWidth;
+        if(direction === "right"){
+            projectContainer.style.transform = "translate(-" + (projectWidth * currentLength) + "px)";
+        }else{
+            projectContainer.style.transform = "translate(-" + (projectWidth * currentLength) + "px)";
+        }
     }
-    select = 0;
-    addClass(0);
-});
-ballSelection[1].addEventListener("click", (e) =>{
-    debugger
-    if(currentLength < (projects.length - showingProjects)){
-        currentLength += 1;
-        UpdateCarousel("right");
-    }
-    select = 1;
-    addClass(1);
-});
 
-function UpdateCarousel (direction){
-    debugger
-    var projectWidth = projectDiv.clientWidth;
-    if(direction === "right"){
-        projectContainer.style.transform = "translateX(-" + (projectWidth * currentLength) + "px)";
-    }else{
-        projectContainer.style.transform = "translateX(-" + (projectWidth * currentLength) + "px)";
-    }
-}
-if(select === 0 || select === 1){
     window.addEventListener("resize", () => {
-        debugger
         setTimeout(() => {
             if(currentLength > 0){
                 currentLength -= 1;
@@ -72,10 +64,9 @@ if(select === 0 || select === 1){
             offset();
             if(select === 1){
                 UpdateCarousel("right");
-            }else{
+            }else if(select === 0){
                 UpdateCarousel("left");
             }
-        }, 600);
-        
+        }, 600);     
     });
-}
+})()
